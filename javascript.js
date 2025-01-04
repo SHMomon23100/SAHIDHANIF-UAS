c5460e40790542b29e314803ba7a4907e5ba2993
SHMomon23100 <sahdhnif@gmail.com>
2025-01-05 00:57:45 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const menuicon = document.getElementById("menu-icon");
+const menulist = document.getElementById("menu-list");
+
+menuicon.addEventListener("click", () => {
+    menulist.classList.toggle("hidden");
+
+});
+
+const menusubmit = document.getElementById("submit")
